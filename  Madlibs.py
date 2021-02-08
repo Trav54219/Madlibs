840e2df8ee03c6d9e5db55e6eb7051d1f4cb06b0
Travis <78454854+Trav54219@users.noreply.github.com>
2021-02-07 20:40:07 -0600

    Add files via upload

---
+++  Madlibs.py
@@ -0,0 +1,14 @@
+#  Welcome to Madlibs
+
+adj=input("adjective: ")
+verb1=input("verb: ")
+verb2=input("verb: ")
+verb3=input("verb: ")
+Famous_Ballplayer=input("Famous ballplayer")
+
+
+
+madlib=f("NBA Player {Famous_Ballplayer} is the basketball player in the world \ He {verb1} his opponents over {verb2} jumpshots over them then he {verb3} on them with such speed and power!")
+
+print(madlib)
+
