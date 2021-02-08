795fd5accc1093a9e841197acf9b22182b042513
Travis <78454854+Trav54219@users.noreply.github.com>
2021-02-07 20:54:08 -0600

    Update Madlibs.py

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+#  Welcome to Madlibs
+
+Superlative=input("Superlative: ")
+verb1=input("verb: ")
+verb2=input("verb: ")
+verb3=input("verb: ")
+Famous_Ballplayer=input("Famous ballplayer")
+
+
+
+madlib=("NBA Player {} is the {} basketball player in the world \ He {} his opponents over {} jumpshots over them then he {} on them with such speed and power!")
+
+print(madlib.format(Famous_Ballplayer, Superlative,verb1,verb2,verb3))
+
